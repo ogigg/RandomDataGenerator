aa61b416cb60d65a4757010df6051b7a0b17a31f
ogigg <gierszon10@gmail.com>
2018-07-01 21:52:03 +0200

    random first and last name generated, click to copy to clipboard added

--- RandomDataGenerator/RandomDataGenerator/Form1.Designer.cs
+++ RandomDataGenerator/RandomDataGenerator/Form1.Designer.cs
@@ -30,31 +30,50 @@
         {
             this.btn_RandAll = new System.Windows.Forms.Button();
             this.textB_FirstName = new System.Windows.Forms.TextBox();
+            this.textB_LastName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_RandAll
             // 
-            this.btn_RandAll.Location = new System.Drawing.Point(1126, 12);
+            this.btn_RandAll.Location = new System.Drawing.Point(844, 10);
+            this.btn_RandAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_RandAll.Name = "btn_RandAll";
-            this.btn_RandAll.Size = new System.Drawing.Size(134, 39);
+            this.btn_RandAll.Size = new System.Drawing.Size(100, 32);
             this.btn_RandAll.TabIndex = 0;
             this.btn_RandAll.Text = "Random All";
             this.btn_RandAll.UseVisualStyleBackColor = true;
+            this.btn_RandAll.Click += new System.EventHandler(this.btn_RandAll_Click);
             // 
             // textB_FirstName
             // 
-            this.textB_FirstName.Location = new System.Drawing.Point(25, 20);
+            this.textB_FirstName.Location = new System.Drawing.Point(19, 16);
+            this.textB_FirstName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textB_FirstName.Name = "textB_FirstName";
-            this.textB_FirstName.Size = new System.Drawing.Size(278, 22);
+            this.textB_FirstName.ReadOnly = true;
+            this.textB_FirstName.Size = new System.Drawing.Size(210, 20);
             this.textB_FirstName.TabIndex = 1;
+            this.textB_FirstName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textB_FirstName_MouseClick);
+            this.textB_FirstName.TextChanged += new System.EventHandler(this.textB_FirstName_TextChanged);
+            // 
+            // textB_LastName
+            // 
+            this.textB_LastName.Location = new System.Drawing.Point(19, 40);
+            this.textB_LastName.Margin = new System.Windows.Forms.Padding(2);
+            this.textB_LastName.Name = "textB_LastName";
+            this.textB_LastName.ReadOnly = true;
+            this.textB_LastName.Size = new System.Drawing.Size(210, 20);
+            this.textB_LastName.TabIndex = 2;
+            this.textB_LastName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textB_LastName_MouseClick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 511);
+            this.ClientSize = new System.Drawing.Size(1020, 280);
+            this.Controls.Add(this.textB_LastName);
             this.Controls.Add(this.textB_FirstName);
             this.Controls.Add(this.btn_RandAll);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -67,6 +86,7 @@
 
         private System.Windows.Forms.Button btn_RandAll;
         private System.Windows.Forms.TextBox textB_FirstName;
+        private System.Windows.Forms.TextBox textB_LastName;
     }
 }
 
--- RandomDataGenerator/RandomDataGenerator/Form1.cs
+++ RandomDataGenerator/RandomDataGenerator/Form1.cs
@@ -13,15 +13,40 @@ namespace RandomDataGenerator
 {
     public partial class Form1 : Form
     {
+        string[] FirstName_S = File.ReadAllText("FirstNames.txt", Encoding.Default).Split('\n');
+        string[] LastName_S = File.ReadAllText("LastNames.txt", Encoding.Default).Split('\n');
         public Form1()
         {
             InitializeComponent();
-            textB_FirstName.Text = File.ReadAllText("FirstNames.txt");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void textB_FirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_RandAll_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            textB_FirstName.Text = FirstName_S[rnd.Next(0,100)];
+            textB_LastName.Text = LastName_S[rnd.Next(0, 100)];
+        }
+
+        private void textB_FirstName_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textB_FirstName.Text!="")
+                Clipboard.SetText(textB_FirstName.Text);
+        }
+
+        private void textB_LastName_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textB_FirstName.Text != "")
+                Clipboard.SetText(textB_LastName.Text);
         }
     }
 }
